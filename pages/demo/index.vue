0d71bb2539e0459c641cef1519f553676c7d3eb8
Thanh Chung Bui <thanhchungbtc@gmail.com>
2020-05-21 07:12:00 +0900

    update

--- pages/demo/index.vue
+++ pages/demo/index.vue
@@ -6,42 +6,38 @@
           <h1 class="page-title">Demo projects</h1>
         </b-col>
       </b-row>
-
     </b-container>
     <div class="container posts">
-
       <b-row>
         <b-col md="5">
           <a href="/vue-shopping" target="_blank">
-
             <picture>
-              <img src="/demo/react-site.png" alt=""/>
+              <img src="/demo/react-site.png" alt />
             </picture>
           </a>
         </b-col>
         <b-col md="7">
-          <a href="https://react-shopping-live.herokuapp.com/" class="text-left text-decoration-none">
+          <a
+            href="https://react-shopping-live.herokuapp.com/"
+            class="text-left text-decoration-none"
+          >
             <h3>React shopping site</h3>
           </a>
-          <p>
-            An example of clean architecture implement in vue.
-          </p>
+          <p>A full-fledge shopping app written in React, react hook, saga, firebase and more</p>
           <p>
             <a href="https://react-shopping-live.herokuapp.com/">Demo</a>
           </p>
           <p>
             <a href="https://github.com/thanhchungbtc/react-shopping-live">Source code</a>
           </p>
-
         </b-col>
       </b-row>
-      <hr>
+      <hr />
       <b-row>
         <b-col md="5">
           <a href="/vue-shopping" target="_blank">
-
             <picture>
-              <img src="/demo/vue-shopping.png" alt=""/>
+              <img src="/demo/vue-shopping.png" alt />
             </picture>
           </a>
         </b-col>
@@ -49,26 +45,20 @@
           <a href="/vue-shopping" class="text-left text-decoration-none">
             <h3>Shopping cart vue</h3>
           </a>
-          <p>
-            An example of clean architecture implement in vue.
-          </p>
+          <p>An example of clean architecture implement in vue.</p>
           <p>
             <a href="/vue-shopping">Demo</a>
           </p>
           <p>
             <a href="https://github.com/thanhchungbtc/vue-shopping-clean-architecture">Source code</a>
           </p>
-
-
         </b-col>
       </b-row>
-      <hr>
-
-
+      <hr />
     </div>
   </div>
 </template>
 
 <script>
-  export default {}
+export default {};
 </script>
